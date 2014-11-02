1c6f4dd28c055edb050baf42a610b6305364e64d
Wei-liang Huang <William@Wei-liangs-Air.hsd1.wa.comcast.net>
2014-11-01 23:04:22 -0700

    testing passed

--- server.js
+++ server.js
@@ -1,2 +1,70 @@
+// // - a route that will display the local time
+
+// // - a route that takes a first name as part of the URL and greets that name.
+
+// // For instance, if the URL specifies a name of 'Zaphod' it should send a JSON object that looks like this:
+
+// // {"msg" : "Hello Zaphod!”}
+
+// BASE SETUP
+// ==============================================
+var moment  = require('moment');
 var express = require('express');
-//your code here
+var app     = express();
+var port    = process.env.PORT || 3000;
+
+// ROUTES
+// ==============================================
+// get an instance of router
+var router = express.Router();
+
+// route middleware that will happen on every request
+router.use(function(req, res, next) {
+
+  // log each request to the console
+  console.log(req.method, req.url);
+
+  // continue doing what we were doing and go to the route
+  next();
+});
+
+app.get('/', function(req, res, next) {
+  var time = '<br><a href="/api/time"><br>Time</a><a href="/api"><br>api page</a>';
+  var name = '<a href="/api/hello/Travis"><br>hello/Travis</a>';
+  res.status(200);
+  res.send('you\'ve reached the index page, click for ' + time + name);
+  next();
+});
+
+router.get('/', function(req, res, next) {
+  var time = '<br><a href="/api/time"><br>Time</a><a href="/api"><br>api page</a>';
+  var name = '<a href="/api/hello/Travis"><br>hello/Travis</a>';
+  res.status(200);
+  res.send('you\'ve visited the api home page, use ' + time + name);
+  next();
+});
+
+// route with parameters (http://localhost:3000/hello/:name)
+router.get('/hello/:name', function(req, res, next) {
+  res.status(200);
+  res.json({
+    msg: 'hello ' + req.params.name + '!'
+  });
+  next();
+});
+
+router.get('/time', function(req, res, next) {
+  res.status(200);
+  res.json({
+    time: moment().format('YYYY-MM-DD HH:mm')
+  });
+  next();
+});
+
+// apply the routes to our application
+app.use('/api', router);
+
+// START THE SERVER
+// ==============================================
+app.listen(port);
+console.log('server started on ' + port);
--- test/api_test.js
+++ test/api_test.js
@@ -1,8 +1,38 @@
 var chai = require('chai');
 var chaihttp = require('chai-http');
+var moment = require('moment');
+var server = 'http://localhost:' + (process.env.PORT || 3000);
+var expect = chai.expect;
+var assert = chai.assert;
+
 chai.use(chaihttp);
 
 describe('Simple JSON API', function() {
-  it('should send the local time');
-  it('should greet someone');
+  it('should send the local time', function(done) {
+    var time = moment().format('YYYY-MM-DD HH:mm');
+    chai.request(server)
+      .get('/api/time')
+      .end(function(err, res) {
+        assert.equal(err, null);
+        expect(res).to.be.a('object', 'not an object');
+        expect(res).to.have.status(200, 'status code not 200');
+        expect(res.body).to.have.property('time');
+        expect(res.body.time).to.have.string(time,'wrong time');
+        done();
+      });
+  });
+
+  it('should greet someone', function(done) {
+    var random = Math.random();
+    chai.request(server)
+      .get('/api/hello/'+random)
+      .end(function(err, res) {
+        assert.equal(err, null);
+        expect(res).to.be.a('object', 'not an object');
+        expect(res).to.have.status(200, 'status code not 200');
+        expect(res.body).to.have.property('msg');
+        expect(res.body.msg).equals('hello '+random+'!','wrong name');
+        done();
+      });
+  });
 });
